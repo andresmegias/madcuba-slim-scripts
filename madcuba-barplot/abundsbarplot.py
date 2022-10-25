a638d894443cc2d7c6561a30408eb9ddcf6b416f
Andrés Megías <109989219+andresmegias@users.noreply.github.com>
2022-10-25 13:00:07 +0200

    Update abundsbarplot.py

--- madcuba-barplot/abundsbarplot.py
+++ madcuba-barplot/abundsbarplot.py
@@ -20,7 +20,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
-config_file = 'barplot4r.yaml'
+config_file = 'example/barplot3.yaml'
 
 import os
 import re
